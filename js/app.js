614417c5a33e6be5da6c29094e6019b26e02d482
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-11 14:47:50 +0600

    phone added

--- js/app.js
+++ js/app.js
@@ -3,12 +3,26 @@ const loadPhones = async() => {
 
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data);
+    displayPhones(data.data);
 
 }
 
-const displayPhones = phone =>{
+const displayPhones = phones =>{
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        const phoneDiv =  document. createElement('div');
+        phoneDiv. classList. add('col');
+        phoneDiv. innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text b   as a natural lead-in to additional content. This content is a li   bit longer.</p>
+        </div>
+        </div>
+        `
+        phonesContainer. appendChild(phoneDiv);
+    });
 
 }
-
 loadPhones();
